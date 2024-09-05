f1d24c1bb3356710d95f40018196676e5703509c
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2024-09-05 15:21:47 +0700

    add running command of project

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,10 +8,16 @@ require('dotenv').config()
 import "./scripts/tasks/deploy.ts";
 import "./scripts/tasks/gen.ts";
 import "./scripts/tasks/mint.ts";
+import "./scripts/tasks/loop.ts";
 import "./scripts/tasks/transfer.ts";
 import "./scripts/tasks/xferEth.ts";
 
-  
+// Acoount config
+const OPERATORS: Array<string> = String(process.env.OPERATORS_KEY).split(" ");
+const PROXIES: Array<string> = String(process.env.PROXIES_KEY).split(" ");
+const USERS: Array<string> = String(process.env.USERS).split(" ");
+const accounts: Array<string> = OPERATORS.concat(PROXIES.concat(USERS));
+
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
   settings: {
@@ -24,17 +30,26 @@ const config: HardhatUserConfig = {
   networks: {
     conduit: {
       url: 'https://rpc-pmtest-6lw45cjbgl.t.conduit.xyz',
-      accounts: [''],
+      accounts: accounts,
     },
     arbitrum_l2: {
       url: 'https://sepolia-rollup.arbitrum.io/rpc',
-      accounts: [''],
+      accounts: accounts,
 	},
+    cardona: {
+        url: "https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public",
+        accounts: accounts,
+    },
+    custom: {
+        url: "http://18.119.5.142:8449",
+        accounts: accounts,
+    },
   },
   etherscan: {
     apiKey: {
-      arbitrum_l2: "abcxzy",
+      arbitrum_l2: "FNCTNQB169UAYDPFGW4EHQ7R8JDNITVIDN",
       conduit:  "abcxzy",
+      cardona: "DVSJS7JQM8N5JJV91GJAZ9SUNFRHAHIJ2G"
     },
     customChains: [
       {
@@ -52,7 +67,15 @@ const config: HardhatUserConfig = {
           apiURL: "https://explorer-pmtest-6lw45cjbgl.t.conduit.xyz/api",
           browserURL: "https://explorer-pmtest-6lw45cjbgl.t.conduit.xyz/"
         }
-      }
+      },
+      {
+        network: "cardona",
+        chainId: 2442,
+        urls: {
+            apiURL: "https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public",
+            browserURL: "https://cardona-zkevm.polygonscan.com/",
+        },
+    },
     ],
   },
   ignition: {
